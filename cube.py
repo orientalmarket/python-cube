f0753a8e7c1a447b01f128bc91c2e7adfd30276f
Nick <chungknick@gmail.com>
2024-08-01 16:45:47 -0400

    rotation matrices

--- cube.py
+++ cube.py
@@ -1,30 +1,72 @@
 import pygame
 import numpy as np
+import logging
 
-colors = {"white": (0, 0, 0), "black": (255, 255, 255)}
+logging.basicConfig(
+    filename="log.txt",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+logging.debug("DEBUGGING")
+
+
+angle_x = angle_y = angle_z = 0
+angle_x = 0.05
+angle_y = 0.05
+angle_z = 0.01
+
+rotation_x = np.array(
+    [
+        [1, 0, 0],
+        [0, np.cos(angle_x), -np.sin(angle_x)],
+        [0, np.sin(angle_x), np.cos(angle_x)],
+    ]
+)
+
+rotation_y = np.array(
+    [
+        [np.cos(angle_y), 0, np.sin(angle_y)],
+        [0, 1, 0],
+        [-np.sin(angle_y), 0, np.cos(angle_y)],
+    ]
+)
+
+rotation_z = np.array(
+    [
+        [np.cos(angle_z), -np.sin(angle_z), 0],
+        [np.sin(angle_z), np.cos(angle_z), 0],
+        [0, 0, 1],
+    ]
+)
+
+
+def calculate_x(i, j, k):
+    vector = np.array([i, j, k])
+    vector_rot_x = np.matmul(vector, rotation_x)
+    vector_rot_y = np.matmul(vector_rot_x, rotation_y)
+    vector_rot_z = np.matmul(vector_rot_y, rotation_z)
+    logging.info(vector_rot_z[2])
+    return vector_rot_z[0]
+
+
+def calculate_y(i, j, k):
+    vector = np.array([i, j, k])
+    vector_rot_x = np.matmul(vector, rotation_x)
+    vector_rot_y = np.matmul(vector_rot_x, rotation_y)
+    vector_rot_z = np.matmul(vector_rot_y, rotation_z)
+    return vector_rot_z[1]
+
+
+def calculate_z(i, j, k):
+    vector = np.array([i, j, k])
+    vector_rot_x = np.matmul(vector, rotation_x)
+    vector_rot_y = np.matmul(vector_rot_x, rotation_y)
+    vector_rot_z = np.matmul(vector_rot_y, rotation_z)
+    return vector_rot_z[2]
 
 
 def main():
-    pygame.init()
-    WIN_WIDTH = 800
-    WIN_HEIGHT = 600
-    screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-    pygame.display.set_caption("3D Cube")
-    clock = pygame.time.Clock()
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-
-        clock.tick(60)
-        screen.fill(colors["white"])
-
-    pygame.quit()
+    calculate_x(-1, -1, 1)
 
 
 if __name__ == "__main__":
